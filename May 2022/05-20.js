83d5494963cd28939f47af0c8ac9a40df4eb601f
Maya S <maya.swansbrough@gmail.com>
2022-05-20 09:11:15 -0400

    05/20 daily challenge

---
+++ May 2022/05-20.js
@@ -0,0 +1,39 @@
+// Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+// Your task is to create a function that, given a proper first and last name, will return the correct alias.
+// Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+// If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+// Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+// var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+// var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+// aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+// aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+
+function aliasGen(first, last){
+
+    first = first.charAt(0).toUpperCase();
+    last = last.charAt(0).toUpperCase();
+    let alias = ""
+    
+    if(firstName.hasOwnProperty(first) && surname.hasOwnProperty(last)){
+      alias += `${firstName[first]} ${surname[last]}`
+    } 
+    
+    else {
+      alias += "Your name must start with a letter from A - Z."
+    }
+    
+    return alias;
+  }
+
+// refactored
+
+function aliasGen(first, last){
+
+    first = first[0].toUpperCase();
+    last = last[0].toUpperCase();
+    
+    if(firstName[first] && surname[last]){
+      return `${firstName[first]} ${surname[last]}`
+    } 
+    return `Your name must start with a letter from A - Z.`
+  }
